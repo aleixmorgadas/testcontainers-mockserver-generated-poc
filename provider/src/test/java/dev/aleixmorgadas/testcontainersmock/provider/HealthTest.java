6c83af8179f9d2b42a8dc2a1ef4cbebd4d3f1d91
Aleix Morgadas <hello@aleixmorgadas.dev>
2022-12-18 16:55:31 +0100

    added HealthTest

---
+++ provider/src/test/java/dev/aleixmorgadas/testcontainersmock/provider/HealthTest.java
@@ -0,0 +1,13 @@
+package dev.aleixmorgadas.testcontainersmock.provider;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HealthTest {
+
+    @Test
+    void healthTest() {
+        assertTrue(true);
+    }
+}
